bbc01ec74b6ad5f8eb2fb959c03e387d2cca93cb
jQrgen <jorgen.notland@protonmail.com>
2018-04-03 18:29:11 +0200

    create mLocationRequest

--- app/src/main/java/com/example/jqrgen/bggeotest/MainActivity.kt
+++ app/src/main/java/com/example/jqrgen/bggeotest/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.example.jqrgen.bggeotest
 
 import android.Manifest
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
-import android.support.v7.app.AppCompatActivity
+import android.support.v4.app.FragmentActivity
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationRequest
 
-class MainActivity : AppCompatActivity() {
+
+
+
+
+class MainActivity : FragmentActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    /**
+     * Provides access to the Fused Location Provider API.
+     */
+    private var mFusedLocationClient: FusedLocationProviderClient? = null
+
+    /**
+     * Stores parameters for requests to the FusedLocationProviderApi.
+     */
+    private var mLocationRequest: LocationRequest? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,9 +45,48 @@ class MainActivity : AppCompatActivity() {
             requestFineLocationPermission()
         }
 
-        val mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        // createLocationRequest()
 
     }
+
+    /**
+     * Sets up the location request. Android has two location request settings:
+     * `ACCESS_COARSE_LOCATION` and `ACCESS_FINE_LOCATION`. These settings control
+     * the accuracy of the current location. This sample uses ACCESS_FINE_LOCATION, as defined in
+     * the AndroidManifest.xml.
+     *
+     *
+     * When the ACCESS_FINE_LOCATION setting is specified, combined with a fast update
+     * interval (5 seconds), the Fused Location Provider API returns location updates that are
+     * accurate to within a few feet.
+     *
+     *
+     * These settings are appropriate for mapping applications that show real-time location
+     * updates.
+     */
+    private fun createLocationRequest() {
+        mLocationRequest = LocationRequest.create()
+
+        // Sets the desired interval for active location updates. This interval is
+        // inexact. You may not receive updates at all if no location sources are available, or
+        // you may receive them slower than requested. You may also receive updates faster than
+        // requested if other applications are requesting location at a faster interval.
+        // Note: apps running on "O" devices (regardless of targetSdkVersion) may receive updates
+        // less frequently than this interval when the app is no longer in the foreground.
+        mLocationRequest?.interval = UPDATE_INTERVAL
+
+        // Sets the fastest rate for active location updates. This interval is exact, and your
+        // application will never receive updates faster than this value.
+        mLocationRequest?.fastestInterval = FASTEST_UPDATE_INTERVAL
+
+        mLocationRequest?.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        // Sets the maximum time when batched location updates are delivered. Updates may be
+        // delivered sooner than this interval.
+        mLocationRequest?.maxWaitTime = MAX_WAIT_TIME
+    }
+
 
     /**
      * Return the current state of the permissions needed.
