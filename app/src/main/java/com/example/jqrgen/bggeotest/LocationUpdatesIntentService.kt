8f6e446e47cce74c661e62a2801a374bd2ff5019
jQrgen <jorgen.notland@protonmail.com>
2018-04-03 20:30:00 +0200

    update mindskVersion

--- app/src/main/java/com/example/jqrgen/bggeotest/LocationUpdatesIntentService.kt
+++ app/src/main/java/com/example/jqrgen/bggeotest/LocationUpdatesIntentService.kt
@@ -2,12 +2,28 @@ package com.example.jqrgen.bggeotest
 
 import android.app.IntentService
 import android.content.Intent
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.google.android.gms.location.LocationResult
+
+
 
 
 class LocationUpdatesIntentService : IntentService(TAG) {
 
     override fun onHandleIntent(intent: Intent?) {
-
+        if (intent != null) {
+            val action = intent.action
+            if (ACTION_PROCESS_UPDATES.equals(action)) {
+                val result = LocationResult.extractResult(intent)
+                if (result != null) {
+                    val locations = result.locations
+                    Utils().setLocationUpdatesResult(this, locations)
+                    Utils().sendNotification(this, Utils().getLocationResultTitle(this, locations))
+                    Log.i(TAG, Utils().getLocationUpdatesResult(this))
+                }
+            }
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/jqrgen/bggeotest/Utils.kt
+++ app/src/main/java/com/example/jqrgen/bggeotest/Utils.kt
@@ -1,7 +1,19 @@
 package com.example.jqrgen.bggeotest
 
 import android.content.Context
+import android.location.Location
 import android.preference.PreferenceManager
+import java.text.DateFormat
+import java.util.*
+import android.app.NotificationManager
+import android.app.NotificationChannel
+import android.os.Build
+import android.graphics.BitmapFactory
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Intent
+import android.graphics.Color
+import android.support.v4.app.NotificationCompat
 
 
 class Utils{
@@ -21,6 +33,107 @@ class Utils{
     fun getLocationUpdatesResult(context: Context): String {
         return PreferenceManager.getDefaultSharedPreferences(context)
                 .getString(KEY_LOCATION_UPDATES_RESULT, "")
+    }
+
+
+    /**
+     * Posts a notification in the notification bar when a transition is detected.
+     * If the user clicks the notification, control goes to the MainActivity.
+     */
+    fun sendNotification(context: Context, notificationDetails: String) {
+        // Create an explicit content Intent that starts the main Activity.
+        val notificationIntent = Intent(context, MainActivity::class.java)
+
+        notificationIntent.putExtra("from_notification", true)
+
+        // Construct a task stack.
+        val stackBuilder = TaskStackBuilder.create(context)
+
+        // Add the main Activity to the task stack as the parent.
+        stackBuilder.addParentStack(MainActivity::class.java)
+
+        // Push the content Intent onto the stack.
+        stackBuilder.addNextIntent(notificationIntent)
+
+        // Get a PendingIntent containing the entire back stack.
+        val notificationPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        // Get a notification builder that's compatible with platform versions >= 4
+        val builder = NotificationCompat.Builder(context)
+
+        // Define the notification settings.
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+                // In a real app, you may want to use a library like Volley
+                // to decode the Bitmap.
+                .setLargeIcon(BitmapFactory.decodeResource(context.resources,
+                        R.mipmap.ic_launcher))
+                .setColor(Color.RED)
+                .setContentTitle("Location update")
+                .setContentText(notificationDetails)
+                .setContentIntent(notificationPendingIntent)
+
+        // Dismiss notification once the user touches it.
+        builder.setAutoCancel(true)
+
+        // Get an instance of the Notification manager
+        val mNotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Android O requires a Notification Channel.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = context.getString(R.string.app_name)
+            // Create the channel for the notification
+            val mChannel = NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_DEFAULT)
+
+            // Set the Notification Channel for the Notification Manager.
+            mNotificationManager.createNotificationChannel(mChannel)
+
+            // Channel ID
+            builder.setChannelId(CHANNEL_ID)
+        }
+
+        // Issue the notification
+        mNotificationManager.notify(0, builder.build())
+    }
+
+    /**
+     * Returns the title for reporting about a list of [Location] objects.
+     *
+     * @param context The [Context].
+     */
+    fun getLocationResultTitle(context: Context, locations: List<Location>): String {
+        val numLocationsReported = context.resources.getQuantityString(
+                R.plurals.num_locations_reported, locations.size, locations.size)
+        return numLocationsReported + ": " + DateFormat.getDateTimeInstance().format(Date())
+    }
+
+    /**
+     * Returns te text for reporting about a list of  [Location] objects.
+     *
+     * @param locations List of [Location]s.
+     */
+    private fun getLocationResultText(context: Context, locations: List<Location>): String {
+        if (locations.isEmpty()) {
+            return context.getString(R.string.unknown_location)
+        }
+        val sb = StringBuilder()
+        for (location in locations) {
+            sb.append("(")
+            sb.append(location.latitude)
+            sb.append(", ")
+            sb.append(location.longitude)
+            sb.append(")")
+            sb.append("\n")
+        }
+        return sb.toString()
+    }
+
+
+    fun setLocationUpdatesResult(context: Context, locations: List<Location>) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(KEY_LOCATION_UPDATES_RESULT, getLocationResultTitle(context, locations)
+                        + "\n" + getLocationResultText(context, locations))
+                .apply()
     }
 
     companion object {
